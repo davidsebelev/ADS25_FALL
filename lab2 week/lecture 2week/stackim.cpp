ec384a59d98bd4503960479612aadd6ca1559c59
Давид <david.sebelev@gmail.com>
2025-09-17 21:40:36 +0500

    st

--- lab2 week/lecture 2week/stackim.cpp
+++ lab2 week/lecture 2week/stackim.cpp
@@ -1,74 +1,60 @@
 #include<iostream>
 using namespace std;
-// stack lifo last in fist out
+
 
 class Node{
-public:
+    public:
     int data;
     Node* next;
-
-    Node(int data){
-        this->data = data;
+    Node(int x){
+        data = x;
         next = nullptr;
     }
 };
-
-class myStack {
-    // making pointer to the top
+class myStack{
+private:
     Node* top;
-
-    int count;
+    int sz;
 public:
-myStack(){
-    // initially stack is empty
-    top = nullptr;
-    count = 0;
+    myStack(){
+        top = nullptr;
+        sz = 0;
+    }
 
-}
-void push(int x) {
-    Node* temp = new Node(x);
-    temp->next = top;
-    top = temp;
-    count++;
-}
-// deleting top el
-int del(){
-    if(top == nullptr) return -1;
-    Node* temp = top;
-    top = top->next;
-    int val = temp->data;
-    delete temp;
-    return val;
-}
+    void push(int new_el){
+        Node* new_node = new Node(new_el);
+        new_node->next = top;
+        top = new_node;
+        sz++;
+    }
+    int del(){
+        if(top == nullptr){
+            return -1;
+        }
+        Node* temp = top;
+        int value = temp->data;
+        top = top->next;
 
-int peek(){
-    if(top == nullptr) return -1;
-    return top->data;
-}
-bool isEmpty(){
-    return top == nullptr;
-}
-int size(){
-    return count;
-}
+        delete temp;
+        sz--;
+        return value;
+    }
+    int size(){
+        return sz;
+    }
+    bool isEmpty(){
+        return top == nullptr;
+
+    }
+    int peek(){
+        if(!top) return -1;
+        return top->data;
+    }
+
+
 
 };
 
-
-
 int main(){
-    myStack st;
-    int n;
-    cin >> n;
-    while(n--){
-        int x;
-        cin >> x;
-        st.push(x);
-    }
-
-    for(int i = 0 ; i < st.size();i++){
-        cout << st.peek() << " ";
-        st.del();
-    }
     return 0;
 }
